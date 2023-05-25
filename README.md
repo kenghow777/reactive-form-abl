e76787d02096b8cf7ae82825f0ba9f18ca8a5e66
kenghow777 <93174619+kenghow777@users.noreply.github.com>
2023-05-25 18:27:04 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reactive-form-abl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xec3c5)
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Hello from {{name}}!</h1>
+    <a target="_blank" href="https://angular.io/start">
+      Learn more about Angular 
+    </a>
+  `,
+})
+export class App {
+  title = 'reactive';
+  form: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.pattern('[a-zA-Z]*'),
+    ]),
+  });
+}
+
+bootstrapApplication(App);
